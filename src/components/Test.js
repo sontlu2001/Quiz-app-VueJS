639819607869b4fb86469baab0729c61a470fe58
Do Ngoc Son <sondo1@cqtdt.asia>
2023-11-17 17:27:41 +0700

    install vuetify and apexcharts

---
+++ src/components/Test.js
@@ -0,0 +1,116 @@
+new Vue({
+  el: '#app',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data: {
+    
+    series: [{
+      data: 
+    }],
+    chartOptions: {
+      chart: {
+        id: 'area-datetime',
+        type: 'area',
+        height: 350,
+        zoom: {
+          autoScaleYaxis: true
+        }
+      },
+      annotations: {
+        yaxis: [{
+          y: 30,
+          borderColor: '#999',
+          label: {
+            show: true,
+            text: 'Support',
+            style: {
+              color: "#fff",
+              background: '#00E396'
+            }
+          }
+        }],
+        xaxis: [{
+          x: new Date('14 Nov 2012').getTime(),
+          borderColor: '#999',
+          yAxisIndex: 0,
+          label: {
+            show: true,
+            text: 'Rally',
+            style: {
+              color: "#fff",
+              background: '#775DD0'
+            }
+          }
+        }]
+      },
+      dataLabels: {
+        enabled: false
+      },
+      markers: {
+        size: 0,
+        style: 'hollow',
+      },
+      xaxis: {
+        type: 'datetime',
+        min: new Date('01 Mar 2012').getTime(),
+        tickAmount: 6,
+      },
+      tooltip: {
+        x: {
+          format: 'dd MMM yyyy'
+        }
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 100]
+        }
+      },
+    },
+    
+    selection: 'one_year',
+  },
+  methods: {
+    updateData: function(timeline) {
+      this.selection = timeline
+  
+      switch (timeline) {
+        case 'one_month':
+          this.$refs.chart.zoomX(
+            new Date('28 Jan 2013').getTime(),
+            new Date('27 Feb 2013').getTime()
+          )
+          break
+        case 'six_months':
+          this.$refs.chart.zoomX(
+            new Date('27 Sep 2012').getTime(),
+            new Date('27 Feb 2013').getTime()
+          )
+          break
+        case 'one_year':
+          this.$refs.chart.zoomX(
+            new Date('27 Feb 2012').getTime(),
+            new Date('27 Feb 2013').getTime()
+          )
+          break
+        case 'ytd':
+          this.$refs.chart.zoomX(
+            new Date('01 Jan 2013').getTime(),
+            new Date('27 Feb 2013').getTime()
+          )
+          break
+        case 'all':
+          this.$refs.chart.zoomX(
+            new Date('23 Jan 2012').getTime(),
+            new Date('27 Feb 2013').getTime()
+          )
+          break
+        default:
+      }
+    }
+  }
+})
